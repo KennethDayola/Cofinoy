aaecb91a90492393d210e0098c538615b6ac1c30
LeinPerl <leinperl2004@gmail.com>
2025-11-12 16:47:30 +0800

    Updated the required characters for making passwords

--- Cofinoy.Services/ServiceModels/UserViewModel.cs
+++ Cofinoy.Services/ServiceModels/UserViewModel.cs
@@ -19,8 +19,10 @@ namespace Cofinoy.Services.ServiceModels
         public string Email { get; set; }
 
         [Required(ErrorMessage = "This field is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters.")]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*$",
+            ErrorMessage = "Password must include A-Z, a-z, & @#$%.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "This field is required.")]
--- Cofinoy.WebApp/Models/NewPasswordViewModel.cs
+++ Cofinoy.WebApp/Models/NewPasswordViewModel.cs
@@ -5,8 +5,10 @@ namespace Cofinoy.WebApp.Models
     public class NewPasswordViewModel
     {
         [Required(ErrorMessage = "This field is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters.")]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*$",
+            ErrorMessage = "Password must include A-Z, a-z, & @#$%.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "This field is required.")]
