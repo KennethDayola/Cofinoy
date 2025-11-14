537125cfdc7f7c72b7663ee7dd3e189c45b602b0
LeinPerl <leinperl2004@gmail.com>
2025-11-14 13:15:32 +0800

    Added logout modal

---
+++ Cofinoy.WebApp/wwwroot/js/adminLayout.js
@@ -0,0 +1,25 @@
+﻿
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutModal = document.getElementById("logoutConfirmModal");
+    const cancelBtn = document.getElementById("cancelLogoutBtn");
+    const confirmBtn = document.getElementById("confirmLogoutBtn");
+
+    const logoutForm = document.getElementById("logoutForm");
+    const openLogoutBtn = document.getElementById("openLogoutModalBtn");
+
+    openLogoutBtn.addEventListener("click", () => {
+        logoutModal.classList.add("active");
+        document.body.style.overflow = "hidden";
+    });
+
+    const closeModal = () => {
+        logoutModal.classList.remove("active");
+        document.body.style.overflow = "auto";
+    };
+
+    cancelBtn.addEventListener("click", closeModal);
+
+    confirmBtn.addEventListener("click", () => {
+        logoutForm.submit();
+    });
+});
--- Cofinoy.WebApp/wwwroot/js/profile.js
+++ Cofinoy.WebApp/wwwroot/js/profile.js
@@ -191,3 +191,30 @@ function showToast(message, type) {
         }, 300);
     }, 3000);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutModal = document.getElementById("logoutConfirmModal");
+    const cancelBtn = document.getElementById("cancelLogoutBtn");
+    const confirmBtn = document.getElementById("confirmLogoutBtn");
+    const closeBtn = document.getElementById("closeLogoutModal");
+
+    const logoutForm = document.getElementById("logoutForm");
+    const openLogoutBtn = document.getElementById("openLogoutModalBtn");
+
+    openLogoutBtn.addEventListener("click", () => {
+        logoutModal.classList.add("active");
+        document.body.style.overflow = "hidden";
+    });
+
+    const closeModal = () => {
+        logoutModal.classList.remove("active");
+        document.body.style.overflow = "auto";
+    };
+
+    cancelBtn.addEventListener("click", closeModal);
+    closeBtn.addEventListener("click", closeModal);
+
+    confirmBtn.addEventListener("click", () => {
+        logoutForm.submit();
+    });
+});
